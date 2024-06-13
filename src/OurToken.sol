d7d266b7bc17de99f4cecbd2b5aae9393c9d4830
Tully <tully.slattery@gmail.com>
2024-06-13 18:38:24 +0800

    5 done

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -2,8 +2,9 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+//this OurToken contract is an ERC20 token meaning it inherits all the public and internal functions and variables that are defined in the erc20 contract
 contract OurToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("OurToken", "OTK") {
         _mint(msg.sender, initialSupply);
---
+++ test/test_OurTokenTest.t.sol
@@ -0,0 +1,15 @@
+
+
+
+// function testMulticall() public {
+//     ERC20MulticallMock multicallToken = new ERC20MulticallMock();
+//     address[] memory recipients = new address[](2);
+//     recipients[0] = bob;
+//     recipients[1] = alice;
+//     uint256[] memory amounts = new uint256[](2);
+//     amounts[0] = 50;
+//     amounts[1] = 100;
+//     checkReturnValues(multicallToken, recipients, amounts);
+//     assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - amounts[0]);
+//     assertEq(ourToken.balanceOf(alice), STARTING_BALANCE + amounts[1]);
+// }
